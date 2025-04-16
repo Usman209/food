8daa3158a4b6ef694b05ad3a748a003a906940b6
Usman Jamil <UJamil@lmkr.com>
2025-04-17 01:59:23 +0500

    code

--- .medusa/server/public/admin/assets/chunk-GW6TVOAA-Js27BiGK.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./chunk-W7625H47-tObwF2xZ.js";var a=()=>e(),s=()=>e();export{s as a,a as u};
--- symlink.js
+++
@@ -1,9 +0,0 @@
-const symlinkDir = require('symlink-dir');
-const path = require('path');
-
-const from = path.resolve('.medusa/server/public');
-const to = path.resolve('public');
-
-symlinkDir(from, to)
-  .then(() => console.log('✅ Symlink created!!!'))
-  .catch((err) => console.error('❌ Failed to create symlink:', err));
