fe44f9c2e3a659ee881cfe8b0e0e16053aaa3ff8
Usman Jamil <UJamil@lmkr.com>
2025-04-17 00:25:13 +0500

    code

--- symlink.js
+++ symlink.js
@@ -5,5 +5,5 @@ const from = path.resolve('.medusa/server/public');
 const to = path.resolve('public');
 
 symlinkDir(from, to)
-  .then(() => console.log('✅ Symlink created'))
+  .then(() => console.log('✅ Symlink created!!!'))
   .catch((err) => console.error('❌ Failed to create symlink:', err));
